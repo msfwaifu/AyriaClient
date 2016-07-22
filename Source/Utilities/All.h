31a9813e63eab80aa4fb8bb188828756df572b05
Convery <convery10122@gmail.com>
2016-07-22 09:32:36 +0200

    Added some basic utilities.

---
+++ Source/Utilities/All.h
@@ -0,0 +1,15 @@
+/*
+    Initial author: (https://github.com/)Convery for Ayria.se
+    License: MIT
+    Started: 2016-7-22
+    Notes:
+        Includes for all utilities.
+*/
+
+#pragma once
+
+#include "Cryptography\Hashing\FNV1.h"
+#include "Strings\Variadicstring.h"
+#include "Strings\Debugstring.h"
+#include "Buffers\Bytebuffer.h"
+#include "Files\Filesystem.h"
