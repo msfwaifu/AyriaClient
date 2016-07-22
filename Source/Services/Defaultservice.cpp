63fb4878f32c7871fa341a3a9f7817c45b35e125
Convery <convery10122@gmail.com>
2016-07-22 23:19:24 +0200

    Added cache to the default service.

--- Source/Services/Defaultservice.cpp
+++ Source/Services/Defaultservice.cpp
@@ -8,7 +8,16 @@
 
 #include <Configuration\All.h>
 #include <Communication\Messagehandler.h>
+#include <Cache\Cachemanager.h>
 #include <thread>
+
+struct MyTestcache : public Cacheblock
+{
+    virtual void Update() override
+    {
+        PrintFunction();
+    }
+};
 
 struct MyTestservice
 {
@@ -55,11 +64,13 @@ struct MyTestservice
         std::thread(Lambda, arg1).detach();
     }
 
-    // Register the handlers.
+    // Register the handlers and data.
     MyTestservice()
     {
         Messagehandler::Registerhandler(FNV1a_Compiletime_32("Testservice_sync"), Testservice_sync);
         Messagehandler::Registerhandler(FNV1a_Compiletime_32("Testservice_async"), Testservice_async);
+
+        Cachemanager::Registerblock(new MyTestcache(), 5000, FNV1a_Compiletime_32("Testcache"));
     }
 };
 
